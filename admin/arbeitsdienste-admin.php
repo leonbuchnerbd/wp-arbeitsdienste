b1baf7386bdaa8dc7cb77d3836c44d5f77f26583
LeonB <leon.buchner1@gmail.com>
2025-03-12 18:34:37 +0100

    update styling admin page

--- admin/arbeitsdienste-admin.php
+++ admin/arbeitsdienste-admin.php
@@ -22,51 +22,55 @@ function arbeitsdienste_admin_page() {
     ?>
     <div class="wrap">
         <h1>Arbeitsdienst Verwaltung</h1>
-        <table class="wp-list-table widefat fixed striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Aufgabe</th>
-                    <th>Datum</th>
-                    <th>Uhrzeit</th>
-                    <th>Benötigte Helfer</th>
-                    <th>Aktionen</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $arbeitsdienste = get_posts(array(
-                    'post_type' => 'arbeitsdienste',
-                    'posts_per_page' => -1,
-                    'orderby' => 'meta_value',
-                    'meta_key' => 'datum',
-                    'order' => 'ASC'
-                ));
+        <div class="arbeitsdienste-table-wrapper">
+            <table class="arbeitsdienste-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Aufgabe</th>
+                        <th>Datum</th>
+                        <th>Uhrzeit</th>
+                        <th>Benötigte Helfer</th>
+                        <th>Aktionen</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $arbeitsdienste = get_posts(array(
+                        'post_type'      => 'arbeitsdienste',
+                        'posts_per_page' => -1,
+                        'orderby'        => 'meta_value',
+                        'meta_key'       => 'datum',
+                        'order'          => 'ASC'
+                    ));
 
-                if (!empty($arbeitsdienste)) {
-                    foreach ($arbeitsdienste as $dienst) {
-                        $datum = get_post_meta($dienst->ID, 'datum', true);
-                        $time = get_post_meta($dienst->ID, 'uhrzeit', true);
-                        $benoetigte_helfer = get_post_meta($dienst->ID, 'benoetigte_helfer', true);
+                    if (!empty($arbeitsdienste)) {
+                        foreach ($arbeitsdienste as $dienst) {
+                            $datum = get_post_meta($dienst->ID, 'datum', true);
+                            $time = get_post_meta($dienst->ID, 'uhrzeit', true);
+                            $benoetigte_helfer = get_post_meta($dienst->ID, 'benoetigte_helfer', true);
 
-                        echo '<tr>';
-                        echo '<td>' . esc_html($dienst->ID) . '</td>';
-                        echo '<td>' . esc_html($dienst->post_title) . '</td>';
-                        echo '<td>' . esc_html($datum) . '</td>';
-                        echo '<td>' . esc_html($time) . '</td>';
-                        echo '<td>' . esc_html($benoetigte_helfer) . '</td>';
-                        echo '<td>
-                                <a href="' . get_edit_post_link($dienst->ID) . '" class="button button-primary">Bearbeiten</a>
-                                <a href="' . get_delete_post_link($dienst->ID) . '" class="button button-danger">Löschen</a>
-                              </td>';
-                        echo '</tr>';
+                            echo '<tr>';
+                            echo '<td data-label="ID">' . esc_html($dienst->ID) . '</td>';
+                            echo '<td data-label="Aufgabe">' . esc_html($dienst->post_title) . '</td>';
+                            echo '<td data-label="Datum">' . esc_html($datum) . '</td>';
+                            echo '<td data-label="Uhrzeit">' . esc_html($time) . '</td>';
+                            echo '<td data-label="Benötigte Helfer">' . esc_html($benoetigte_helfer) . '</td>';
+                            echo '<td data-label="Aktionen">
+                                    <a href="' . get_edit_post_link($dienst->ID) . '" class="button button-primary">Bearbeiten</a>
+                                    <a href="' . get_delete_post_link($dienst->ID) . '" class="button button-danger">Löschen</a>
+                                  </td>';
+                            echo '</tr>';
+                        }
+                    } else {
+                        echo '<tr><td colspan="6">Keine Arbeitsdienste gefunden.</td></tr>';
                     }
-                } else {
-                    echo '<tr><td colspan="7">Keine Arbeitsdienste gefunden.</td></tr>';
-                }
-                ?>
-            </tbody>
-        </table>
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
     <?php
 }
+?>
+
