3fa26fddab1bd25886680d375a2715d4f9ab9327
LeonB <leon.buchner1@gmail.com>
2025-07-23 16:54:31 +0200

    🚀 Version 2.15

--- includes/version.php
+++ includes/version.php
@@ -9,7 +9,7 @@ if (!defined('ABSPATH') && !defined('ARBEITSDIENSTE_VERSION_STANDALONE')) {
 }
 
 // Plugin Version (temporär auf 2.8 für Demo des Auto-Updates auf 2.9)
-define('ARBEITSDIENSTE_PLUGIN_VERSION', '2.14');
+define('ARBEITSDIENSTE_PLUGIN_VERSION', '2.15');
 
 // Wenn als standalone Script ausgeführt, Version ausgeben
 if (defined('ARBEITSDIENSTE_VERSION_STANDALONE') || (php_sapi_name() === 'cli' && basename($_SERVER['SCRIPT_NAME']) === 'version.php')) {
